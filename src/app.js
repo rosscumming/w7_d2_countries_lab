4451c8b1b30b7b4d1d3a2291d325554ba4053326
Ross <rosso5792@gmail.com>
2020-01-14 14:11:06 +0000

    added html drop down and basic app js structure set up

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: '#app',
+    data: {
+      countryName: "",
+      countryFlag: null
+    },
+    methods: {
+      fetchCountry: function () {
+        fetch()
+      }
+    }
+  })
+
+
+
+
+
+
+
+
+});
